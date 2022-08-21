ce7d51ff37a671943fa76554e1c297a5c0dee710
Lucas Josino <contact@lucasjosino.com>
2022-08-21 11:02:23 -0300

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Your final, deployed URL.
+  //
+  // Astro uses this full URL to generate your sitemap
+  // and canonical URLs in your final build.
+  site: "https://www.my-site.dev",
+  // Specifies the output target for builds.s
+  //
+  // ‘static’ - Building a static site to be deploy to any static host.
+  output: "static",
+});
