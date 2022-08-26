5083f64376f8d5afd81b94fdbf9b2d5519b02b4a
Lucas Josino <contact@lucasjosino.com>
2022-08-26 13:58:06 -0300

    Update configs.
    - Update `host` to true, now `yarn dev` will run on local network;
    - Update `paths` from `tsconfig`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,13 +9,17 @@ export default defineConfig({
   //
   // Astro uses this full URL to generate your sitemap
   // and canonical URLs in your final build.
-  site: "https://www.my-site.dev",
+  site: 'https://www.my-site.dev',
   // Specifies the output target for builds.s
   //
   // ‘static’ - Building a static site to be deploy to any static host.
-  output: "static",
+  output: 'static',
   // Astro sitemap.
   //
   // Ref: https://docs.astro.build/en/guides/integrations-guide/sitemap/
-  integrations: [sitemap(), compress()]
+  integrations: [sitemap(), compress()],
+  // Listen on all addresses, including LAN and public addresses.
+  //
+  // Ref: https://docs.astro.build/en/reference/configuration-reference/#serverhost
+  server: { host: true },
 });
